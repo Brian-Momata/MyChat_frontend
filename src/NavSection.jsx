666286cfeeb0bf77516b3b657bbd5cccb755f6d5
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-10 08:10:27 +0300

    Add structure

--- src/NavSection.jsx
+++ src/NavSection.jsx
@@ -1,8 +1,31 @@
 import './styling/NavSection.css'
+import { useState } from 'react';
 
 const NavSection = () => {
+  const [activeIcon, setActiveIcon] = useState('chats');
+  
+  const handleIconClick = (icon) => {
+    setActiveIcon(icon);
+  };
   return (
-    <nav>Nav</nav>
+    <nav>
+      <ul>
+        <li className={activeIcon === 'chats'? 'active': ''} onClick={() => handleIconClick('chats')}>
+          <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+        </li>
+        <li className={activeIcon === 'people'? 'active': ''} onClick={() => handleIconClick('people')}>
+          <ion-icon name="people-outline"></ion-icon>
+        </li>
+      </ul>
+      <div className='nav-config'>
+        <i className={activeIcon === 'settings'? 'active': ''} onClick={() => handleIconClick('settings')}>
+          <ion-icon name="cog-outline"></ion-icon>
+        </i>
+        <div className='profile'>
+          <img src="" />
+        </div>
+      </div>
+    </nav>
   )
 };
 
