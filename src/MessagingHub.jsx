a3fea923b7437fa32f80493091bad981002db89e
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-11 06:55:07 +0300

    Add the chats structure and style them

--- src/MessagingHub.jsx
+++ src/MessagingHub.jsx
@@ -1,9 +1,36 @@
-import './styling/MessagingHub.css'
+import './styling/MessagingHub.css';
+import { useContext } from 'react';
+import { IconContext } from './App';
 
 const MessagingHub = () => {
+  const { activeIcon } = useContext(IconContext);
+
   return (
-    <div className="messaging-hub">MessagingHub</div>
-  )
+    <div className="messaging-hub">
+      {activeIcon === 'chats' && (
+        <>
+          <div className='hub-header'>
+            <h3>Chats</h3>
+          </div>
+          <div className='hub-chat'>
+            <img className='chat-avatar' src="" alt="profile" />
+            <div className='chat-details'>
+              <div className='chat-info'>
+                <p className='chat-name'>Name</p>
+                <span className='chat-time'>Time</span>
+              </div>
+              <p className='last-message'>Last message</p>
+            </div>
+          </div>
+        </>
+      )}
+      {activeIcon === 'people' && (
+        <div className='hub-header'>
+          <h3>People</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MessagingHub;
