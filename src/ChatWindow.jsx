fafa340a29f4aa829d8c7900728e7ebc00d6f6ab
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-12 10:45:05 +0300

    Add structure

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -1,8 +1,28 @@
 import './styling/ChatWindow.css'
+import { IconContext } from './App';
+import { useContext } from 'react';
 
 const ChatWindow = () => {
+  const { clickedUser } = useContext(IconContext)
   return (
-    <div className="chat-window">ChatWindow</div>
+    <div className="chat-window">
+      <div className="chat-window-profile">
+        <img className='chat-window-avatar' src="" alt="profile" />
+        <div className='user-info'>
+          <h4>{clickedUser.name}</h4>
+          <div className='status-container'>
+            <span className='online-status'>Online</span>
+            <span className='last-seen'>Last seen: {clickedUser.lastseen}</span>
+          </div>
+        </div>
+      </div>
+      <div className="messages-container">
+      </div>
+      <div className="write-messages">
+        <input type="text" placeholder="Type your message..." />
+        <button><ion-icon name="send-outline"></ion-icon></button>
+      </div>
+    </div>
   )
 };
 
