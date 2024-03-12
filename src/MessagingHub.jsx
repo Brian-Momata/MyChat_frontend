eb83dcf1a4a2d91dd1f06227d963c83e98883562
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-12 10:44:45 +0300

    Enable clicks

--- src/MessagingHub.jsx
+++ src/MessagingHub.jsx
@@ -3,7 +3,14 @@ import { useContext } from 'react';
 import { IconContext } from './App';
 
 const MessagingHub = () => {
-  const { activeIcon } = useContext(IconContext);
+  const { activeIcon, handlePersonClick } = useContext(IconContext);
+
+  // I'm using this for the functionality. Will delete later
+  const people = [
+    { name: 'John', lastseen: 'Yesterday, 12:13 PM' },
+    { name: 'Brian', lastseen: 'Yesterday, 09:13 PM' },
+    { name: 'Momata', lastseen: 'Today, 12:13 AM' }
+  ];
 
   return (
     <div className="messaging-hub">
@@ -12,11 +19,11 @@ const MessagingHub = () => {
           <div className='hub-header'>
             <h3>Chats</h3>
           </div>
-          <div className='hub-chat'>
-            <img className='chat-avatar' src="" alt="profile" />
+          <div className='hub-chat' onClick={() => handlePersonClick(people[0])}>
+            <img className='hub-avatar' src="" alt="profile" />
             <div className='chat-details'>
               <div className='chat-info'>
-                <p className='chat-name'>Name</p>
+                <p className='chat-name'>{people[0].name}</p>
                 <span className='chat-time'>Time</span>
               </div>
               <p className='last-message'>Last message</p>
@@ -27,6 +34,24 @@ const MessagingHub = () => {
       {activeIcon === 'people' && (
         <div className='hub-header'>
           <h3>People</h3>
+          <div className='hub-person' onClick={() => handlePersonClick(people[0])}>
+            <img className='hub-avatar' src="" alt="profile" />
+            <div className='person-details'>
+              <p className='person-name'>{people[0].name}</p>
+            </div>
+          </div>
+          <div className='hub-person' onClick={() => handlePersonClick(people[1])}>
+            <img className='hub-avatar' src="" alt="profile" />
+            <div className='person-details'>
+              <p className='person-name'>{people[1].name}</p>
+            </div>
+          </div>
+          <div className='hub-person' onClick={() => handlePersonClick(people[2])}>
+            <img className='hub-avatar' src="" alt="profile" />
+            <div className='person-details'>
+              <p className='person-name'>{people[2].name}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
