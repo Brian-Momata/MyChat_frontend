ce24d460cd47d6a76bc8d8ad709913b0aeed54b3
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-12 10:42:52 +0300

    Add functionality for when user is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,26 @@ import './styling/App.css'
 export const IconContext = createContext({
   activeIcon: '',
   setActiveIcon: () => {},
+  clickedUser: {},
+  handlePersonClick: () => {},
 })
 function App() {
   const [activeIcon, setActiveIcon] = useState('chats');
+  const [clickedUser, setClickedUser] = useState('');
+
+  const handlePersonClick = (person) => setClickedUser(person);
   return (
     <div className="app">
       <div className="header">
         <ion-icon name="logo-wechat"></ion-icon>
         <h4>MyChat</h4>
       </div>
-      <IconContext.Provider value={{ activeIcon, setActiveIcon }}>
+      <IconContext.Provider value={{ 
+          activeIcon,
+          setActiveIcon,
+          clickedUser,
+          handlePersonClick
+        }}>
         <NavSection />
         <MessagingHub />
         <ChatWindow />
