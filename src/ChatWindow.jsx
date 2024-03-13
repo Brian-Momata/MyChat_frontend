3fb7663f6ff7971ae31632e53896c2a457e24f7d
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-13 05:54:54 +0300

    Display dependes on if there's a clickedUser

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -3,27 +3,31 @@ import { IconContext } from './App';
 import { useContext } from 'react';
 
 const ChatWindow = () => {
-  const { clickedUser } = useContext(IconContext)
-  return (
-    <div className="chat-window">
-      <div className="chat-window-profile">
-        <img className='chat-window-avatar' src="" alt="profile" />
-        <div className='user-info'>
-          <h4>{clickedUser.name}</h4>
-          <div className='status-container'>
-            <span className='online-status'>Online</span>
-            <span className='last-seen'>Last seen: {clickedUser.lastseen}</span>
-          </div>
-        </div>
-      </div>
-      <div className="messages-container">
-      </div>
-      <div className="write-messages">
-        <input type="text" placeholder="Type your message..." />
-        <button><ion-icon name="send-outline"></ion-icon></button>
-      </div>
-    </div>
-  )
+	const { clickedUser } = useContext(IconContext)
+	return (
+		<div className="chat-window">
+			{clickedUser && (
+				<>
+					<div className="chat-window-profile">
+						<img className='chat-window-avatar' src="" alt="profile" />
+						<div className='user-info'>
+							<h4>{clickedUser.name}</h4>
+							<div className='status-container'>
+								<span className='online-status'>Online</span>
+								<span className='last-seen'>Last seen: {clickedUser.lastseen}</span>
+							</div>
+						</div>
+					</div>
+					<div className="messages-container">
+					</div>
+					<div className="write-messages">
+						<input type="text" placeholder="Type your message..." />
+						<button><ion-icon name="send-outline"></ion-icon></button>
+					</div>
+				</>
+			)}
+		</div>
+	)
 };
 
 export default ChatWindow;
