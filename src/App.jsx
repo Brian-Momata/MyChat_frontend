a33415259da228175cb5bd1bd6340b9b5ae2e025
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-14 07:43:50 +0300

    Able to authenticate a user

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,60 @@ import React, { createContext, useState } from "react"
 import NavSection from "./NavSection"
 import MessagingHub from "./MessagingHub"
 import ChatWindow from "./ChatWindow"
+import Auth from "./Auth"
 import './styling/App.css'
 
 export const IconContext = createContext({
   activeIcon: '',
-  setActiveIcon: () => {},
+  setActiveIcon: () => { },
   clickedUser: {},
-  handlePersonClick: () => {},
+  handlePersonClick: () => { },
+  onAuthenticationSuccess: () => { },
 })
 function App() {
   const [activeIcon, setActiveIcon] = useState('chats');
   const [clickedUser, setClickedUser] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
 
   const handlePersonClick = (person) => setClickedUser(person);
+
+  const onAuthenticationSuccess = () => {
+    console.log("Authentication successful!");
+    setAuthenticated(true);
+  };
+
   return (
-    <div className="app">
-      <div className="header">
-        <ion-icon name="logo-wechat"></ion-icon>
-        <h4>MyChat</h4>
-      </div>
-      <IconContext.Provider value={{ 
-          activeIcon,
-          setActiveIcon,
-          clickedUser,
-          handlePersonClick
-        }}>
-        <NavSection />
-        <MessagingHub />
-        <ChatWindow />
+    authenticated ? (
+      <>
+        <div className="app">
+          <div className="header">
+            <ion-icon name="logo-wechat"></ion-icon>
+            <h4>MyChat</h4>
+          </div>
+          <IconContext.Provider value={{
+            activeIcon,
+            setActiveIcon,
+            clickedUser,
+            handlePersonClick,
+            onAuthenticationSuccess
+          }}>
+            <NavSection />
+            <MessagingHub />
+            <ChatWindow />
+          </IconContext.Provider>
+        </div>
+      </>
+    ) : (
+      <IconContext.Provider value={{
+        activeIcon,
+        setActiveIcon,
+        clickedUser,
+        handlePersonClick,
+        onAuthenticationSuccess
+      }}>
+        <Auth />
       </IconContext.Provider>
-    </div>
+    )
   )
 }
 
