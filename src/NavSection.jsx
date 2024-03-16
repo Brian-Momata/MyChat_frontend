c1f6964f3721205bcfdd89d4307692f169e83817
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-16 08:46:22 +0300

    Add and style modal for the profile

--- src/NavSection.jsx
+++ src/NavSection.jsx
@@ -1,33 +1,53 @@
 import { IconContext } from './App';
-import './styling/NavSection.css'
+import './styling/NavSection.css';
 import { useContext } from 'react';
 
 const NavSection = () => {
-  const { activeIcon, setActiveIcon } = useContext(IconContext);
-  
+  const { activeIcon, setActiveIcon, showProfile, setShowProfile } = useContext(IconContext);
+
   const handleIconClick = (icon) => {
-    setActiveIcon(icon);
+    if (icon === 'profile') {
+      setShowProfile((prev) => !prev);
+    } else {
+      setActiveIcon(icon);
+      setShowProfile(false);
+    }
   };
+
   return (
     <nav>
       <ul>
-        <li className={activeIcon === 'chats'? 'active': ''} onClick={() => handleIconClick('chats')}>
+        <li className={activeIcon === 'chats' ? 'active' : ''} onClick={() => handleIconClick('chats')}>
           <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
         </li>
-        <li className={activeIcon === 'people'? 'active': ''} onClick={() => handleIconClick('people')}>
+        <li className={activeIcon === 'people' ? 'active' : ''} onClick={() => handleIconClick('people')}>
           <ion-icon name="people-outline"></ion-icon>
         </li>
       </ul>
       <div className='nav-config'>
-        <i className={activeIcon === 'settings'? 'active': ''} onClick={() => handleIconClick('settings')}>
+        <i className={activeIcon === 'settings' ? 'active' : ''} onClick={() => handleIconClick('settings')}>
           <ion-icon name="cog-outline"></ion-icon>
         </i>
-        <div className='profile'>
-          <img src="" />
+        {showProfile && (
+          <div className='profile-modal'>
+            <div className='profile-img-container'>
+              <div className='profile'>
+                <img src="" alt="profile" />
+              </div>
+            </div>
+            <div className='profile-details'>
+              <p>Name: John Doe</p>
+              <p>Email: johndoe@example.com</p>
+            </div>
+          </div>
+        )}
+
+        <div className='nav-profile' onClick={() => handleIconClick('profile')}>
+          <img src="" alt="Profile" />
         </div>
       </div>
     </nav>
-  )
+  );
 };
 
 export default NavSection;
