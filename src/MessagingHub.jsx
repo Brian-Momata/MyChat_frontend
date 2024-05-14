2e85c0323649a2bb29e157d7d0a5a3196d165770
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-05-14 10:10:40 +0300

    Fix displaying last message

--- src/MessagingHub.jsx
+++ src/MessagingHub.jsx
@@ -1,9 +1,36 @@
 import './styling/MessagingHub.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { IconContext } from './App';
 
 const MessagingHub = () => {
-  const { activeIcon, handlePersonClick, users, sentMessages, defaultProfile } = useContext(IconContext);
+  const { activeIcon, handlePersonClick, users, sentMessages, defaultProfile, currentUser } = useContext(IconContext);
+  const [latestMessages, setLatestMessages] = useState([]);
+
+  useEffect(() => {
+    const fetchLatestMessages = async () => {
+      const latestMsgs = await Promise.all(sentMessages.map(async (message) => {
+        const latestMsg = await latestMessage(currentUser.id, message.receiver_id);
+        return { receiverId: message.receiver_id, content: latestMsg.content, time: latestMsg.created_at };
+      }));
+      setLatestMessages(latestMsgs);
+    }
+
+    if (currentUser) {
+      fetchLatestMessages();
+    }
+  }, [currentUser, sentMessages]);
+
+
+  const latestMessage = async (senderId, receiverId) => {
+    try {
+      const response = await fetch(`https://backend-api-dmnv.onrender.com/user/latest_between_users?sender_id=${senderId}&receiver_id=${receiverId}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching latest message:', error);
+      return null;
+    }
+  };
 
   return (
     <div className="messaging-hub">
@@ -13,16 +40,18 @@ const MessagingHub = () => {
             <h3>Chats</h3>
           </div>
           {sentMessages.map((message, index) => {
-            const receiverUser = users.find(user => user.id === message.receiver_id);
+            const receiver = users.find(user => user.id === message.receiver_id);
+            const latestMsg = latestMessages.find(msg => msg.receiverId === receiver.id);
+            console.table(latestMessages)
             return (
-              <div key={index} className='hub-chat' onClick={() => handlePersonClick(receiverUser)}>
-                <img className='hub-avatar' src={receiverUser.avatar || defaultProfile} alt="profile" />
+              <div key={index} className='hub-chat' onClick={() => handlePersonClick(receiver)}>
+                <img className='hub-avatar' src={receiver.avatar || defaultProfile} alt="profile" />
                 <div className='chat-details'>
                   <div className='chat-info'>
-                    <p className='chat-name'>{receiverUser?.username}</p>
-                    <span className='chat-time'>{new Date(message.created_at).toLocaleTimeString()}</span>
+                    <p className='chat-name'>{receiver?.username}</p>
+                    <span className='chat-time'>{new Date(latestMsg?.time).toLocaleTimeString()}</span>
                   </div>
-                  <p className='last-message'>{message.content}</p>
+                  <p className='last-message'>{latestMsg?.content}</p>
                 </div>
               </div>
             );
